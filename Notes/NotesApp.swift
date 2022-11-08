e0755b0c72639670817bd410cb05a27d3d368fb3
brian419 <json10@crimson.ua.edu>
2022-11-07 19:45:19 -0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Brian Son on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
